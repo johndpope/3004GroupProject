6cb7e7ce0122d578c4e3511a29f520083db1d1e5
Connor Hicks <connor@agilebits.com>
2016-10-03 13:11:44 -0400

    Added Xcode skeleton

---
+++ StreamIt/StreamIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StreamIt
+//
+//  Created by Connor MacBook on 2016-10-03.
+//  Copyright © 2016 3004Group. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
