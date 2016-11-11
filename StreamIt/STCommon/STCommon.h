c852542a499e17ff05240464887bf489fb271fa9
agilecohix <connor@agilebits.com>
2016-11-11 14:24:36 -0500

    Added constants, STCommon framework, bonjour manager

---
+++ StreamIt/STCommon/STCommon.h
@@ -0,0 +1,19 @@
+//
+//  STCommon.h
+//  STCommon
+//
+//  Created by Connor MacBook on 2016-11-11.
+//  Copyright © 2016 3004Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for STCommon.
+FOUNDATION_EXPORT double STCommonVersionNumber;
+
+//! Project version string for STCommon.
+FOUNDATION_EXPORT const unsigned char STCommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <STCommon/PublicHeader.h>
+
+#import "STCommon-Swift.h"
