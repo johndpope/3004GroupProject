f64866ca208f7600fd65450c4207d2d5e2506ce4
Jagger De Leo <jagger27.com@gmail.com>
2016-11-11 19:41:08 -0500

    Added asyncronous HTTP hello world framework, fixed (hopefully) remaining Swift 3.0->2.3 issues

--- StreamIt/STCommon/STCommon.h
+++ StreamIt/STCommon/STCommon.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char STCommonVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <STCommon/PublicHeader.h>
 
-#import "STCommon-Swift.h"
+//#import "STCommon-Swift.h"
---
+++ StreamIt/STHTTPServer/STHTTPServer.h
@@ -0,0 +1,20 @@
+//
+//  STHTTPServer.h
+//  STHTTPServer
+//
+//  Created by Jagger De Leo on 2016-11-11.
+//  Copyright © 2016 3004Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for STHTTPServer.
+FOUNDATION_EXPORT double STHTTPServerVersionNumber;
+
+//! Project version string for STHTTPServer.
+FOUNDATION_EXPORT const unsigned char STHTTPServerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <STHTTPServer/PublicHeader.h>
+
+
+#import "STHTTPServer-Swift.h"
