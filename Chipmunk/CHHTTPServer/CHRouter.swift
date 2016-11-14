394836a9cf33b15cca840f5a336a3e43b6b45a1a
Jagger De Leo <jagger27.com@gmail.com>
2016-11-14 12:16:08 -0500

    Router improvements (POST and JSON support)

--- Chipmunk/CHHTTPServer/CHRouter.swift
+++ Chipmunk/CHHTTPServer/CHRouter.swift
@@ -8,17 +8,18 @@
 
 import Foundation
 import GCDWebServer
+import CHDatabase
 
 public class CHRouter : NSObject {
     
     static func helloWorldHandler() -> GCDWebServerAsyncProcessBlock {
-        return  {(request: GCDWebServerRequest!, completion: GCDWebServerCompletionBlock!) in
+        return {(request: GCDWebServerRequest!, completion: GCDWebServerCompletionBlock!) in
             completion(GCDWebServerDataResponse(HTML: "<html><p>Hello, World!</p></html>")!)
         }
     }
     
     static func defaultHandler() -> GCDWebServerAsyncProcessBlock {
-        return{(request: GCDWebServerRequest!, completion: GCDWebServerCompletionBlock!) in
+        return {(request: GCDWebServerRequest!, completion: GCDWebServerCompletionBlock!) in
             let res: GCDWebServerResponse = GCDWebServerErrorResponse(HTML: "404 Page not found")
             res.statusCode = 404
             completion(res)
@@ -26,9 +27,26 @@ public class CHRouter : NSObject {
     }
     
     static func indexHandler() -> GCDWebServerAsyncProcessBlock {
-        return{(request: GCDWebServerRequest!, completion: GCDWebServerCompletionBlock!) in
+        return {(request: GCDWebServerRequest!, completion: GCDWebServerCompletionBlock!) in
             completion(GCDWebServerDataResponse(HTML:
                 "<p style='font-family:sans-serif'>Let's call it Chipmunk.</p>"))
+        }
+    }
+    
+    static func clientHandler() -> GCDWebServerAsyncProcessBlock {
+        return {(request: GCDWebServerRequest!, completion: GCDWebServerCompletionBlock!) in
+            guard let dataReq = request as? GCDWebServerDataRequest else {
+                return
+            }
+            print(dataReq.data)
+            let newClient = CHClient()
+            newClient.username = "Victor"
+            
+            
+            print(request)
+          //  GCDWebServerDataRequest
+            completion(GCDWebServerDataResponse(JSONObject: ["test":123]))
+            
         }
     }
 }
